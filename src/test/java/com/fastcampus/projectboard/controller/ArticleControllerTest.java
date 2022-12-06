cebd01156fd9d53c81040b60c589bf3ad4c71a49
bang-star <bang77060986@gmail.com>
2022-12-06 18:52:53 +0900

    #49 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` → ` userId`로 id를 변경하는 내용을 모든 자바코드에 반영

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -219,7 +219,7 @@ class ArticleControllerTest {
     }
 
     private UserAccountDto createUserAccountDto() {
-        return UserAccountDto.of(1L,
+        return UserAccountDto.of(
                 "artist",
                 "pw",
                 "artist@mail.com",
