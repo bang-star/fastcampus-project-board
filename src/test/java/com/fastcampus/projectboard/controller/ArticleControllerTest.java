78c60d45e62e4703775debe9780ebcc622c9b63b
bang-star <bang77060986@gmail.com>
2022-12-07 13:13:02 +0900

    #51 - 리팩토링: 오타 수정, 중복 import 제거, 불필요한 태그 제거
    * xmlns:th태그 제거
    * 오타 변경
    * 중복 import 제거

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -334,7 +334,7 @@ class ArticleControllerTest {
                 "artist",
                 "pw",
                 "artist@mail.com",
-                "Uartistno",
+                "artist",
                 "memo",
                 LocalDateTime.now(),
                 "artist",
