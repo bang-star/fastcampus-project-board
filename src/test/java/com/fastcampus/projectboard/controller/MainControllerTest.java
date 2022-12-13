581987832e882f0fdcac1fc77daca0c3d86c12cf
bang-star <bang77060986@gmail.com>
2022-12-13 15:15:43 +0900

    #71 - 불필요한 Import, 띄어쓰기 제거
    프로젝트 편집 중 실수가 있었던 것으로 보여 수정

--- src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
