b0ca27904ae561cacbca1b44d28a385639839272
bang-star <bang77060986@gmail.com>
2022-12-21 18:42:09 +0900

    #84 - Principal: OAuth 인증 정보를 다룰 수 있도록 기능 업데이트
    `OAuth2User` 인터페이스를 구현하여 Spring OAuth Client .도 기존과 동일하게
    인증 정보를 인식할 수 있도록 함.

--- src/main/java/com/fastcampus/projectboard/dto/security/BoardPrincipal.java
+++ src/main/java/com/fastcampus/projectboard/dto/security/BoardPrincipal.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,13 +18,18 @@ public record BoardPrincipal(
         Collection<? extends GrantedAuthority> authorities,
         String email,
         String nickname,
-        String memo
-) implements UserDetails {
+        String memo,
+        Map<String, Object> oAuth2Attributes
+) implements UserDetails, OAuth2User {
 
     public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
+        return of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo, Map<String, Object> oAuth2Attributes) {
 
         /**
-         * 지금은 인증만 하고 권한을 다루고 있지 않아서 임의로 세팅한다.
+         * TODO 지금은 인증만 하고 권한을 다루고 있지 않아서 임의로 세팅한다.
          * by Mr.Bang 2022.12.14
          */
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
@@ -37,7 +44,8 @@ public record BoardPrincipal(
                 ,
                 email,
                 nickname,
-                memo
+                memo,
+                oAuth2Attributes
         );
     }
 
@@ -71,6 +79,9 @@ public record BoardPrincipal(
     @Override public boolean isCredentialsNonExpired() { return true; }
     @Override public boolean isEnabled() { return true; }
 
+
+    @Override public String getName() { return username; }
+    @Override public Map<String, Object> getAttributes() { return oAuth2Attributes; }
 
     public enum RoleType {
         USER("ROLE_USER");
